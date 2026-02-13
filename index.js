bcf9b19349617f0cec57c1ff08ee4817daff6e1d
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2026-02-13 15:13:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(8000, () => {
+  console.log(`🚀 Server running on port ${8000}`);
+});
+
+// export default app;
