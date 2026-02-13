d16d051bfb82c1909ecf0d6700878157c284db73
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2026-02-13 15:14:24 -0300

    Change health endpoint to root endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
     timestamp: new Date().toISOString(),
